f1cd5e0c8357cc150e265632975fdb189a96868e
Khaliid-H634 <Pr3414ob@go.minnstate.edu>
2026-05-28 21:27:42 -0500

    Week-02-reflection

--- app/src/main/java/edu/metrostate/ics342/mediatracker/navigation/Routes.kt
+++ app/src/main/java/edu/metrostate/ics342/mediatracker/navigation/Routes.kt
@@ -6,10 +6,13 @@ object Routes {
     const val ACTIVITY_FEED = "activity_feed"
     const val SEARCH        = "search"
     const val LIBRARY       = "library"
+
     const val CONNECTIONS   = "connections"
     const val MY_PROFILE    = "my_profile"
     const val SETTINGS      = "settings"
-    const val MEDIA_DETAIL  = "detail"
+    //const val MEDIA_DETAIL  = "detail"
+    const val MEDIA_DETAIL  = "media_detail/{mediaId}"
+
     const val WRITE_REVIEW  = "write_review/{mediaId}"
     const val USER_PROFILE  = "user_profile/{userId}"
     const val EDIT_PROFILE  = "edit_profile"
